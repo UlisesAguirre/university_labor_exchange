da4003f281b09d787a800fddf86fbf5d50bd85fa
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-09-25 15:58:52 -0300

    fixed: routes profile

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import UserMenu from '../../components/UserMenu/UserMenu'
+import React from 'react';
+import UserMenu from '../../components/UserMenu/UserMenu';
+import MenuCard from '../../components/MenuCard/MenuCard';
+import { Route, Routes } from 'react-router-dom';
+import UserCard from '../../components/UserCard/UserCard';
 
-import "./userPage.css"
-import MenuCard from '../../components/MenuCard/MenuCard'
+import './userPage.css';
 
 const UserPage = () => {
   return (
     <div className='userPage-container'>
-        <UserMenu />
-        <MenuCard />
+      <UserMenu />
+      <MenuCard>
+        <Routes>
+          <Route path='/' element={<UserCard />} />
+        </Routes>
+      </MenuCard>
     </div>
-  )
+  );
 }
 
-export default UserPage
+export default UserPage;
