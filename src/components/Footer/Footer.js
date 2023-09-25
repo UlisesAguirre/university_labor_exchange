ca1cd3c4752f55b6a324a5960db8ea503082042b
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-09-25 16:01:10 -0300

    change: integrate themeButton in footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import "./footer.css"
+import ThemeButton from '../ThemeButton/ThemeButton'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-        Derechos reservados &copy; PPS
+      <ThemeButton/>
+      <p>Derechos reservados &copy; PPS</p>
+      <button style={{opacity: 0}}>Aca va el chatbot</button>
     </div>
   )
 }
