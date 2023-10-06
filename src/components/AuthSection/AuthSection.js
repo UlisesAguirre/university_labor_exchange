650c69c8f59bceb9f5cbbe349ce80b588f3b784d
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-10-06 12:18:18 -0300

    add: button to redirect profile

--- src/components/AuthSection/AuthSection.js
+++ src/components/AuthSection/AuthSection.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { faRightToBracket, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faRightToBracket, faPenToSquare, faUser } from '@fortawesome/free-solid-svg-icons';
 import UserButton from '../Shared/UserButton/UserButton'
 import "./authSection.css"
 import UserContext from '../Context/UserContext/UserContext';
@@ -9,11 +9,14 @@ const AuthSection = () => {
   const { user } = useContext(UserContext);
 
   return (
-    <div className='authSection-container'>
-      {!user &&
+    <div className={!user ? 'authSection-container' : 'authSection-logged-container' }>
+      {!user ?
         <>
           <UserButton to="/login" buttonName="Iniciar sesion" icon={faRightToBracket} />
           <UserButton to="/signup" buttonName="Registrarse" icon={faPenToSquare} />
+        </> :
+        <>
+        <UserButton to="/profile" buttonName={user.username} icon={faUser} />
         </>}
     </div>
   )
